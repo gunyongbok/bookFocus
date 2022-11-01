e1c6b401bcfd9da714335a6d71b40a561e773eb4
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-01 23:02:40 +0900

    make booklists at mainn

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -2,9 +2,14 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+const BookList = styled.ul``;
+
+const Books = styled.li``;
+
 function Main({ data }) {
     let navigate = useNavigate();
-
+    const a = Object.entries(data)[3][1];
+    console.log(a[0].bookReportId);
     return (
         <div>
             <button
@@ -14,6 +19,13 @@ function Main({ data }) {
             >
                 독서록 쓰기
             </button>
+            <BookList>
+                {data['result'].map((a, i) => (
+                    <Books key={data['result'][i].bookReportId}>
+                        {data['result'][i].title}
+                    </Books>
+                ))}
+            </BookList>
         </div>
     );
 }
