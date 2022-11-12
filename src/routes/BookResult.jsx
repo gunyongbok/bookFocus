ba884e134167ff0aa167e4890f7e31798aabb5ef
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-12 22:38:41 +0900

    Feat: 독서록 결과 페이지에 해당하는 정보 제공
    독서록 제목,책 제목,작가,작성한 책 내용 불러오기

--- src/routes/BookResult.jsx
+++ src/routes/BookResult.jsx
@@ -6,23 +6,29 @@ import styles from '../css/BookResult.module.css';
 function BookResult({ data }) {
     const { id } = useParams();
     const navigate = useNavigate();
-    console.log(id);
+
+    const dataResult = data.result;
+    console.log(dataResult);
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>
                 <img className={styles.book_img} src={book1} />
-                <h1>bookId : {id}</h1>
+                {dataResult.map((a) => {
+                    if (a.bookReportId === Number(id)) {
+                        return (
+                            <div key={a.bookReportId}>
+                                <h1>{a.title}</h1>
+                                <h2>{a.bookTitle}</h2>
+                                <h4>{a.author}</h4>
+                                <p>{a.content}</p>
+                            </div>
+                        );
+                    }
+                })}
                 <img className={styles.book_img} src={book2} />
             </div>
             <span>
-                <button
-                    onClick={() => {
-                        navigate('/bookresult');
-                    }}
-                >
-                    완성된 독서록 보러가기
-                </button>
                 <button
                     onClick={() => {
                         navigate('/');
